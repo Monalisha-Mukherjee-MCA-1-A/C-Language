1db7e65f17963d62b3a8bd5b6145718026d4ad4b
MONALISHAMUKHERJEE_MCA_2024-26_27_UEMK <145867630+Monalisha-Mukherjee-MCA-1-A@users.noreply.github.com>
2025-01-19 14:04:54 +0530

    Create sumavgnum.c

---
+++ sumavgnum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() 
+{
+numbers. 
+int a, b, c; 
+float sum, avg; 
+printf("Enter three numbers: "); 
+scanf("%d %d %d", &a, &b, &c);
+sum = a + b + c; 
+avg = sum / 3; 
+printf("Sum = %.2f\n", sum); 
+printf("Average = %.2f\n", avg);
+return 0;
+}
